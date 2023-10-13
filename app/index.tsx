fe4d98b2b3aae037ade7ff61f1752f69f988784e
Max Wo <hello@maxwowo.com>
2023-10-13 17:15:26 +1300

    feat: use safe area view

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { FunctionComponent } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { Button, Stack, Text } from 'tamagui'
 
 const Index: FunctionComponent = () => {
   return (
-    <Stack alignItems="center" justifyContent="center">
-      <Text>Bruh</Text>
-      <Button>Button</Button>
-    </Stack>
+    <SafeAreaView>
+      <Stack alignItems="center" justifyContent="center">
+        <Text>Bruh</Text>
+        <Button>Button</Button>
+      </Stack>
+    </SafeAreaView>
   )
 }
 
