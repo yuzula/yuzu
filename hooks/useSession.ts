2cb9fe8309ced3d1ddf8c0e4d041fc246effa35c
Max Wo <hello@maxwowo.com>
2023-10-13 13:07:41 +1300

    feat: implement useSession hook

--- hooks/useSession.ts
+++ hooks/useSession.ts
@@ -1,22 +1,38 @@
-import { Session } from '@supabase/supabase-js'
-import { useEffect, useState } from 'react'
+import { AuthError, Session } from '@supabase/supabase-js'
+import { useCallback, useEffect, useState } from 'react'
 
 import { supabase } from '../clients/supabase'
 
 const useSession = () => {
   const [session, setSession] = useState<Session | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<AuthError | null>(null)
+
+  const doGetSession = useCallback(async () => {
+    setIsLoading(true)
+
+    const result = await supabase.auth.getSession()
+
+    if (result.error) {
+      setError(result.error)
+    } else {
+      setSession(result.data.session)
+    }
+
+    setIsLoading(false)
+  }, [])
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+    doGetSession()
+  }, [doGetSession])
 
+  useEffect(() => {
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
     })
   }, [])
 
-  return session
+  return { session, isLoading, error }
 }
 
 export default useSession
