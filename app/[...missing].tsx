68d75763ea1b0122664cfe284a4a071bcc336cf6
Max Wo <hello@maxwowo.com>
2023-10-14 13:37:55 +1300

    feat: home screen

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -1,17 +1,31 @@
-import { Link } from 'expo-router'
-import React, { FunctionComponent } from 'react'
-import { Text, View } from 'react-native'
+import { useRouter } from 'expo-router'
+import React, { FunctionComponent, useCallback } from 'react'
+import { Pressable, Text, View } from 'react-native'
 
-const NotFound: FunctionComponent = () => (
-  <View>
-    <Text>You ain&apos;t supposed to be here!</Text>
-    <Text>
-      You&apos;ve somehow landed on a screen that shouldn&apos;t exist
-    </Text>
-    <Link href="/">
-      <Text>Take me home</Text>
-    </Link>
-  </View>
-)
+const NotFound: FunctionComponent = () => {
+  const router = useRouter()
+
+  const handleHomeButtonPress = useCallback(() => {
+    router.push('/')
+  }, [router])
+
+  return (
+    <View className="flex-1 items-center justify-center space-y-2">
+      <Text className="font-Poppins_700Bold text-xl">
+        You ain&apos;t supposed to be here!
+      </Text>
+      <Text className="font-Poppins_400Regular text-lg">
+        You&apos;ve somehow landed on a screen that shouldn&apos;t exist
+      </Text>
+      <Pressable className="active:opacity-90" onPress={handleHomeButtonPress}>
+        <View className="rounded-lg bg-blue-600 p-4">
+          <Text className="font-Poppins_600SemiBold text-white">
+            Take me home
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  )
+}
 
 export default NotFound
--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'expo-router'
 import { FunctionComponent, useCallback } from 'react'
 import { Pressable, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -5,21 +6,30 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import Logo from '../components/Logo'
 
 const Index: FunctionComponent = () => {
-  const handleLoginPress = useCallback(() => {}, [])
+  const router = useRouter()
 
-  const handleSignUpPress = useCallback(() => {}, [])
+  const handleLoginButtonPress = useCallback(() => {
+    router.push('/login')
+  }, [router])
+
+  const handleRegisterButtonPress = useCallback(() => {
+    router.push('/register')
+  }, [router])
 
   return (
     <SafeAreaView className="flex-1 bg-primary" edges={['top']}>
-      <SafeAreaView className="flex-1 bg-indigo-500" edges={['bottom']}>
+      <SafeAreaView className="flex-1 bg-purple-600" edges={['bottom']}>
         <View className="flex-1 basis-2 items-center justify-between bg-primary">
           <View className="grow items-center justify-center">
             <Logo height={120} width={120} />
           </View>
 
           <View className="w-full grow justify-end">
-            <Pressable className="active:opacity-90" onPress={handleLoginPress}>
-              <View className="h-20 items-center justify-center bg-blue-500">
+            <Pressable
+              className="active:opacity-90"
+              onPress={handleLoginButtonPress}
+            >
+              <View className="h-20 items-center justify-center bg-blue-600">
                 <Text className="font-Poppins_600SemiBold text-xl text-white">
                   LOG IN
                 </Text>
@@ -28,9 +38,9 @@ const Index: FunctionComponent = () => {
 
             <Pressable
               className="active:opacity-90"
-              onPress={handleSignUpPress}
+              onPress={handleRegisterButtonPress}
             >
-              <View className="h-20 items-center justify-center bg-indigo-500">
+              <View className="h-20 items-center justify-center bg-purple-600">
                 <Text className="font-Poppins_600SemiBold text-xl text-white">
                   SIGN UP
                 </Text>
