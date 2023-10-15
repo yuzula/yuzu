0a1e16fbf355c1b6b20b7e7dc1a5ef68d9dc1771
Max Wo <hello@maxwowo.com>
2023-10-16 11:33:00 +1300

    refactor: add comment

--- app.d.ts
+++ app.d.ts
@@ -1,2 +1,5 @@
+// We need this here to stop VS Code from complaining about the type
+// of `className`
+//
 // https://www.nativewind.dev/getting-started/typescript
 /// <reference types="nativewind/types" />
