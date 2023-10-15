60ef9f34e2fa0c67a8c1f3f1d2cedfee45d06070
Max Wo <hello@maxwowo.com>
2023-10-15 23:01:54 +1300

    feat: get splash screen working

--- App.tsx
+++ App.tsx
@@ -1,21 +1,76 @@
+import { FontAwesome5 } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import * as SplashScreen from 'expo-splash-screen'
-import { StatusBar } from 'expo-status-bar'
-import React, { FunctionComponent, memo, useEffect } from 'react'
+import React, { FunctionComponent, memo, useCallback } from 'react'
 
 import useFonts from './hooks/useFonts'
 import useSession from './hooks/useSession'
-import Navigation from './navigation'
+import linking from './navigation/linking'
+import EmailVerification from './screens/EmailVerification'
+import Home from './screens/home'
+import Login from './screens/Login'
+import Profile from './screens/profile'
+import Register from './screens/Register'
+import Root from './screens/Root'
+import { RootStackParamList, RootTabParamList } from './types'
 
 SplashScreen.preventAutoHideAsync()
 
+const BottomTab = createBottomTabNavigator<RootTabParamList>()
+
+const BottomTabNavigator: FunctionComponent = () => {
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'rgb(0, 122, 255)',
+        tabBarInactiveTintColor: 'rgb(142, 142, 147)',
+        tabBarStyle: {
+          backgroundColor: 'white'
+        }
+      }}
+    >
+      <BottomTab.Screen
+        component={Home}
+        name="Home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 color={color} name="home" size={20} />
+          )
+        }}
+      />
+      <BottomTab.Screen
+        component={Profile}
+        name="Profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 color={color} name="user-alt" size={20} />
+          )
+        }}
+      />
+    </BottomTab.Navigator>
+  )
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
 const App: FunctionComponent = memo(() => {
   const { isLoading: areFontsLoading, error: fontsError } = useFonts()
-  const { isLoading: isSessionLoading, error: sessionError } = useSession()
+  const {
+    session,
+    isLoading: isSessionLoading,
+    error: sessionError
+  } = useSession()
 
   const areResourcesLoading = areFontsLoading || isSessionLoading
   const areResourcesErroring = fontsError || sessionError
 
-  useEffect(() => {
+  const handleNavigationReady = useCallback(() => {
     if (!areResourcesLoading && !areResourcesErroring) {
       SplashScreen.hideAsync()
     }
@@ -25,10 +80,25 @@ const App: FunctionComponent = memo(() => {
     return null
   } else {
     return (
-      <>
-        <StatusBar />
-        <Navigation />
-      </>
+      <NavigationContainer linking={linking} onReady={handleNavigationReady}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {session ? (
+            <>
+              <Stack.Screen component={BottomTabNavigator} name="Tabs" />
+            </>
+          ) : (
+            <>
+              <Stack.Screen component={Root} name="Root" />
+              <Stack.Screen component={Register} name="Register" />
+              <Stack.Screen component={Login} name="Login" />
+              <Stack.Screen
+                component={EmailVerification}
+                name="EmailVerification"
+              />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 })
