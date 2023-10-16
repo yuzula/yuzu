1c31286f375bc9a1da1f8154715cd0b3298c07c5
Max Wo <hello@maxwowo.com>
2023-10-17 09:07:55 +1300

    feat: dynamic member count

--- screens/home.tsx
+++ screens/home.tsx
@@ -2,6 +2,7 @@ import { User } from '@supabase/supabase-js'
 import React, { FunctionComponent, useEffect, useState } from 'react'
 import { ActivityIndicator, Alert, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { z } from 'zod'
 
 import { supabase } from '../clients/supabase'
 import { GENERIC_ERROR_MESSAGE, GENERIC_ERROR_TITLE } from '../constants/alert'
@@ -10,6 +11,7 @@ import { ProfileSchema, profileSchema } from '../models/profile'
 const Home: FunctionComponent = () => {
   const [user, setUser] = useState<User>()
   const [profile, setProfile] = useState<ProfileSchema>()
+  const [memberCount, setMemberCount] = useState<number>()
 
   useEffect(() => {
     const doGetUser = async () => {
@@ -49,18 +51,36 @@ const Home: FunctionComponent = () => {
     doGetProfile()
   }, [user])
 
+  useEffect(() => {
+    const doGetMemberCount = async () => {
+      if (profile) {
+        try {
+          const memberCount = await supabase.rpc('count_members', {
+            domain_name: profile.community_domain_name
+          })
+
+          setMemberCount(z.number().parse(memberCount.data))
+        } catch (error) {
+          Alert.alert(GENERIC_ERROR_TITLE, GENERIC_ERROR_MESSAGE)
+        }
+      }
+    }
+
+    doGetMemberCount()
+  }, [profile, user])
+
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      {!profile ? (
+      {!profile || !memberCount ? (
         <ActivityIndicator />
       ) : (
         <View className="w-5/6 flex-1 pt-6">
           <View className="space-y-2">
             <Text className="font-Poppins_700Bold text-xl">
-              {profile?.community_domain_name}
+              {profile.community_domain_name}
             </Text>
-            <Text className="font-Poppins_400Regular text-apple-gray-light">
-              452 members
+            <Text className="font-Poppins_500Medium text-apple-gray-light">
+              {`${memberCount} ${memberCount > 1 ? 'members' : 'member'}`}
             </Text>
           </View>
         </View>
