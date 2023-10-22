43549cb8dd707856ed8f9c9a9ddbc52deeb51f75
Max Wo <hello@maxwowo.com>
2023-10-22 14:58:52 +1300

    fix: types

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -271,10 +271,7 @@ const Home: FunctionComponent<RootTabScreenProps<'Home'>> = ({
     async (post: postModel.Schema) => {
       try {
         if (user) {
-          await reportService.reportPost({
-            postId: post.id,
-            reporterId: user.id
-          })
+          await reportService.reportPost(post.id)
 
           Alert.alert('Post has been reported for moderation', undefined, [
             {
