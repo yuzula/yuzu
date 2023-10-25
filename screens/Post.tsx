f242e2bd55963076bb68995a13ad88938c00b09a
Max Wo <hello@maxwowo.com>
2023-10-25 17:37:16 +1300

    feat: make comments less tall

--- screens/Post.tsx
+++ screens/Post.tsx
@@ -407,7 +407,7 @@ const Post: FunctionComponent<RootStackScreenProps<'Post'>> = ({
               </>
             }
             renderItem={item => (
-              <View className="space-y-1 py-4">
+              <View className="space-y-1 py-2">
                 <View className="mx-auto w-5/6 flex-row items-center justify-between">
                   <View className="flex-row items-center space-x-2">
                     <Text className="font-Poppins_500Medium">
