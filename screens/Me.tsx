2bcc8f75e27bc0d044a67c69870e92ad4fdd8454
Max Wo <hello@maxwowo.com>
2023-10-26 20:41:32 +1300

    feat: me page

--- screens/Me.tsx
+++ screens/Me.tsx
@@ -1,14 +1,24 @@
 import { useActionSheet } from '@expo/react-native-action-sheet'
-import React, { FunctionComponent, useCallback } from 'react'
-import { Alert, View } from 'react-native'
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState
+} from 'react'
+import { ActivityIndicator, Alert, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { supabase } from '../clients/supabase'
 import Button from '../components/Button'
-import { GENERIC_ERROR_MESSAGE } from '../constants/alert'
+import { GENERIC_ERROR_MESSAGE, GENERIC_ERROR_TITLE } from '../constants/alert'
+import useCurrentUser from '../hooks/useCurrentUser'
+import * as profileModel from '../models/profile'
+import * as profileService from '../services/profile'
 
 const Me: FunctionComponent = () => {
   const { showActionSheetWithOptions } = useActionSheet()
+  const user = useCurrentUser()
+  const [profile, setProfile] = useState<profileModel.Schema>()
 
   const handleLogOutButtonPress = useCallback(async () => {
     showActionSheetWithOptions(
@@ -33,11 +43,52 @@ const Me: FunctionComponent = () => {
     )
   }, [showActionSheetWithOptions])
 
+  useEffect(() => {
+    ;(async () => {
+      if (user) {
+        try {
+          setProfile(await profileService.get(user.id))
+        } catch (error) {
+          Alert.alert(GENERIC_ERROR_TITLE, GENERIC_ERROR_MESSAGE)
+        }
+      }
+    })()
+  }, [user])
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <View className="w-5/6">
-        <Button onPress={handleLogOutButtonPress}>Log Out</Button>
-      </View>
+    <SafeAreaView
+      className="flex-1 items-center justify-center bg-white py-6"
+      edges={['top']}
+    >
+      {!user || !profile ? (
+        <ActivityIndicator />
+      ) : (
+        <View className="mx-auto w-5/6 flex-1 justify-between space-y-4">
+          <View className="space-y-4">
+            <Text
+              className="font-Poppins_600SemiBold text-2xl underline decoration-primary"
+              style={{ textDecorationStyle: 'double' }}
+            >
+              Logged in as:&nbsp;
+              <Text>{profile.username}</Text>
+            </Text>
+
+            <Text className="font-Poppins_600SemiBold text-2xl">
+              Number of posts: 13
+            </Text>
+            <Text className="font-Poppins_600SemiBold text-2xl">
+              Number of replies: 15
+            </Text>
+            <Text className="font-Poppins_600SemiBold text-2xl">
+              Total upvotes: 19
+            </Text>
+          </View>
+          <View className="space-y-2">
+            <Button variant="secondary">Delete my account</Button>
+            <Button onPress={handleLogOutButtonPress}>Log Out</Button>
+          </View>
+        </View>
+      )}
     </SafeAreaView>
   )
 }
