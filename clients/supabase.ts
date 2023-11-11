4ab08e5e4da4abdec61cfba4fb02e5836932a203
Max Wo <hello@maxwowo.com>
2023-11-11 18:17:20 +1300

    feat: put secrets in .env

--- clients/supabase.ts
+++ clients/supabase.ts
@@ -2,13 +2,13 @@ import 'react-native-url-polyfill/auto'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
-import { z } from 'zod'
 
 import { Database } from '../types/supabase'
 
 export const supabase = createClient<Database>(
-  z.string().parse(process.env.SUPABASE_URL),
-  z.string().parse(process.env.SUPABASE_KEY),
+  process.env.SUPABASE_URL || 'https://sgqmlhvwdcvykcrgjnbk.supabase.co',
+  process.env.SUPABASE_KEY ||
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNncW1saHZ3ZGN2eWtjcmdqbmJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMDU5NDMsImV4cCI6MjAxMjY4MTk0M30.2fRJPZqnEHYrOWP7rvaNq0rk8kGZDJpwhXRc39QjuOE',
   {
     auth: {
       storage: AsyncStorage,
