a0fd12880cdb3570d2d0d0d01058b8dc7df15ef4
Max Wo <hello@maxwowo.com>
2023-11-13 20:36:38 +1300

    feat: dummy search screen

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent } from 'react'
-import { Text } from 'react-native'
+import React, { FunctionComponent } from 'react'
+import { Text, TextInput, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { RootTabScreenProps } from '../types'
@@ -10,7 +10,18 @@ export const Search: FunctionComponent<RootTabScreenProps<'Search'>> = () => {
       className="flex-1 items-center justify-center bg-white"
       edges={['top']}
     >
-      <Text>Search</Text>
+      <View className="w-full items-center justify-center space-y-2 border-b border-gray-100 py-4">
+        <TextInput
+          autoFocus
+          className="mx-auto w-5/6 rounded-xl bg-gray-100 p-2 font-Poppins_600SemiBold"
+          placeholder="Search communities"
+          returnKeyType="search"
+        />
+      </View>
+
+      <View className="flex-1 items-center justify-center">
+        <Text>body</Text>
+      </View>
     </SafeAreaView>
   )
 }
