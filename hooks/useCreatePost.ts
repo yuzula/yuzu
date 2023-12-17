5e351892e81b7906c5421b103b66634115b42d65
Max Wo <hello@maxwowo.com>
2023-12-18 01:05:41 +1300

    feat: use invalidate query

--- hooks/useCreatePost.ts
+++ hooks/useCreatePost.ts
@@ -1,10 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import * as Haptics from 'expo-haptics'
 import * as Sentry from 'sentry-expo'
-import { z } from 'zod'
 
 import { NotAuthenticatedError } from '../errors/NotAuthenticatedError'
-import { postModel } from '../models/post'
 import { postService } from '../services/post'
 import { useProfileContext } from './useProfileContext'
 
@@ -36,56 +34,8 @@ export const useCreatePost = () => {
         isPrivate
       })
     },
-    // Insert the new post at the very top of feeds so users can see their newly-created
-    // post more obviously
-    onSuccess: async post => {
-      queryClient.setQueriesData(
-        { queryKey: ['posts'], exact: true },
-        (prevPostsData: unknown) => {
-          const prevPosts = postModel.schema
-            .array()
-            .optional()
-            .parse(prevPostsData)
-
-          return prevPosts ? [post, ...prevPosts] : prevPosts
-        }
-      )
-
-      queryClient.setQueriesData(
-        {
-          queryKey: ['posts', 'community', post.community_domain_name],
-          predicate: query => {
-            const queryKeyParams = z
-              .object({
-                sortBy: z.string(),
-                filterBy: z.string()
-              })
-              .optional()
-              .parse(query.queryKey[3])
-
-            if (!queryKeyParams) {
-              return false
-            }
-
-            if (queryKeyParams.filterBy === 'all') {
-              return true
-            }
-
-            return (
-              (post.is_private && queryKeyParams.filterBy === 'private') ||
-              (!post.is_private && queryKeyParams.filterBy === 'public')
-            )
-          }
-        },
-        (prevPostsData: unknown) => {
-          const prevPosts = postModel.schema
-            .array()
-            .optional()
-            .parse(prevPostsData)
-
-          return prevPosts ? [post, ...prevPosts] : prevPosts
-        }
-      )
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
 
       await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success)
     },
