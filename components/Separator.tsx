824d7a202e901ddbf86dbfd87de07be670abf23e
Max Wo <hello@maxwowo.com>
2023-12-22 19:32:06 +1300

    feat: make separator thicker

--- components/Separator.tsx
+++ components/Separator.tsx
@@ -2,5 +2,5 @@ import React, { FunctionComponent } from 'react'
 import { View, ViewProps } from 'react-native'
 
 export const Separator: FunctionComponent<ViewProps> = () => (
-  <View className="w-full border-t border-gray-200" />
+  <View className="h-1 w-full bg-gray-200" />
 )
