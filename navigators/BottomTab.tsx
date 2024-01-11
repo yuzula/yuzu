987d2f666e942433aba6a43fe93c592f5c6308a3
Max Wo <hello@maxwowo.com>
2024-01-11 13:47:22 +1300

    feat: play haptics on bottom tab navigation

--- navigators/BottomTab.tsx
+++ navigators/BottomTab.tsx
@@ -2,6 +2,7 @@ import { FontAwesome5 } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import * as Haptics from 'expo-haptics'
 import React, { FunctionComponent } from 'react'
+import { useBoolean } from 'usehooks-ts'
 
 import { Community } from '../screens/Community'
 import { Home } from '../screens/Home'
@@ -11,65 +12,76 @@ import { RootTabParamList } from '../types'
 
 const BottomTab = createBottomTabNavigator<RootTabParamList>()
 
-export const BottomTabNavigator: FunctionComponent = () => (
-  <BottomTab.Navigator
-    initialRouteName="Home"
-    screenListeners={{
-      blur: async () => {
-        await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
-      }
-    }}
-    screenOptions={{
-      headerShown: false,
-      tabBarActiveTintColor: 'black',
-      tabBarInactiveTintColor: 'rgb(142, 142, 147)',
-      tabBarStyle: {
-        backgroundColor: 'white'
-      },
-      tabBarLabelStyle: {
-        fontFamily: 'Poppins_600SemiBold'
-      }
-    }}
-  >
-    <BottomTab.Screen
-      component={Home}
-      name="Home"
-      options={{
-        title: 'Home',
-        tabBarIcon: ({ color }) => (
-          <FontAwesome5 color={color} name="home" size={20} />
-        )
+export const BottomTabNavigator: FunctionComponent = () => {
+  const {
+    value: isInitialStateChange,
+    setFalse: setIsInitialStateChangeFalse
+  } = useBoolean(true)
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      screenListeners={{
+        state: async () => {
+          if (!isInitialStateChange) {
+            await Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+          }
+
+          setIsInitialStateChangeFalse()
+        }
       }}
-    />
-    <BottomTab.Screen
-      component={Community}
-      name="Community"
-      options={{
-        title: 'Community',
-        tabBarIcon: ({ color }) => (
-          <FontAwesome5 color={color} name="users" size={20} />
-        )
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'rgb(142, 142, 147)',
+        tabBarStyle: {
+          backgroundColor: 'white'
+        },
+        tabBarLabelStyle: {
+          fontFamily: 'Poppins_600SemiBold'
+        }
       }}
-    />
-    <BottomTab.Screen
-      component={Search}
-      name="Search"
-      options={{
-        title: 'Search',
-        tabBarIcon: ({ color }) => (
-          <FontAwesome5 color={color} name="search" size={20} />
-        )
-      }}
-    />
-    <BottomTab.Screen
-      component={Me}
-      name="Me"
-      options={{
-        title: 'Me',
-        tabBarIcon: ({ color }) => (
-          <FontAwesome5 color={color} name="user-alt" size={20} />
-        )
-      }}
-    />
-  </BottomTab.Navigator>
-)
+    >
+      <BottomTab.Screen
+        component={Home}
+        name="Home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 color={color} name="home" size={20} />
+          )
+        }}
+      />
+      <BottomTab.Screen
+        component={Community}
+        name="Community"
+        options={{
+          title: 'Community',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 color={color} name="users" size={20} />
+          )
+        }}
+      />
+      <BottomTab.Screen
+        component={Search}
+        name="Search"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 color={color} name="search" size={20} />
+          )
+        }}
+      />
+      <BottomTab.Screen
+        component={Me}
+        name="Me"
+        options={{
+          title: 'Me',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 color={color} name="user-alt" size={20} />
+          )
+        }}
+      />
+    </BottomTab.Navigator>
+  )
+}
